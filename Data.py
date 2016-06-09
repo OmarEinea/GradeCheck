b70a70c202c553fd95dfa401bea9dad20cdf4cbf
OmarEinea <eineao@gmail.com>
2016-06-09 16:24:38 +0300

    Add template file to store data

---
+++ Data.py
@@ -0,0 +1,18 @@
+"""
+This is a template file to store user specific information,
+run this command: $ git update-index --assume-unchanged Data.py
+so that you can update it with your info and keep them private.
+"""
+
+# Store username in format "uXXXXXXXX"
+username = ""
+
+# Store password in format: "XXXXXXXX"
+password = ""
+
+# Store the current term in format: "Semester Year-Year"
+term = ""
+
+# Store courses you already know their grade
+# to prevent showing them every time
+known_courses = []
